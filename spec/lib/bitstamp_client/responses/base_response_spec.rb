b54c250a917c92732065e9c097e0628399a66770
Ramon Tayag <ramon.tayag@gmail.com>
2019-01-07 11:47:42 +0800

    All responses are not successful when Bitstamp's response has a `status` key

--- spec/lib/bitstamp_client/responses/base_response_spec.rb
+++ spec/lib/bitstamp_client/responses/base_response_spec.rb
@@ -8,5 +8,17 @@ module BitstampClient
       it { is_expected.to have_attribute(:body, String) }
     end
 
+    context "response status is 200 but 'status' field is 'error'" do
+      subject(:response) { described_class.new(raw_response: raw_response) }
+      let(:body) do
+        {status: "when this is filled it's already error"}.to_json
+      end
+      let(:raw_response) do
+        instance_double(Typhoeus::Response, success?: true, body: body)
+      end
+
+      it { is_expected.to_not be_success }
+    end
+
   end
 end
